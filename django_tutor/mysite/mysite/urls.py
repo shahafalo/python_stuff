bb2e95bd30ee35c0552a4421fd54ae3b99aa0e99
shahafalo <shahafalo@gmail.com>
2018-01-31 19:25:01 +0200

    while Django tutorial part 3

--- django_tutor/mysite/mysite/urls.py
+++ django_tutor/mysite/mysite/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from polls import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^polls/', include('polls.urls')),
+    url(r'^pools/', views.pools, name='pools'),
 ]
+
