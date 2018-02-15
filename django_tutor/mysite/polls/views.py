6f6ae7b2447bbc9090ab4f0ffd3ca731dbc71067
shahaf <shahafalo@gmail.com>
2018-02-15 17:58:52 +0200

    while django tutor 3

--- django_tutor/mysite/polls/views.py
+++ django_tutor/mysite/polls/views.py
@@ -1,11 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.template import loader
+from .models import Question
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    response = HttpResponse(template.render(context, request))
+    response.write("<title>dogy</title>")
+    return response
 
 
 def detail(request, question_id):
