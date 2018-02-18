60ee9dc1caa153ae0a917ad20ba4e10d9f805855
shahaf <shahafalo@gmail.com>
2018-02-18 19:01:21 +0200

    finish django tutor part 3

--- django_tutor/mysite/polls/views.py
+++ django_tutor/mysite/polls/views.py
@@ -2,7 +2,10 @@
 from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.template import loader
+from django.http import Http404
 from .models import Question
+from django.shortcuts import render
+
 
 
 # Create your views here.
@@ -11,14 +14,18 @@ def index(request):
     template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list': latest_question_list,
+        'title': "Question view"
     }
     response = HttpResponse(template.render(context, request))
-    response.write("<title>dogy</title>")
     return response
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question, 'title': "question " + question_id})
 
 
 def results(request, question_id):
